19b903332260389d8bf86d6dfbcad6cd3711c702
ztk37 <ztk37.contact@gmail.com>
2022-01-23 19:14:59 +0100

    setup nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const IndexPage: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Demorga | Website </title>
+        <meta name="description" content="Demorga's Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Website for Demorga will launch here 🚀</h1>
+
+        <p>IndexPage</p>
+      </main>
+    </div>
+  );
+};
+
+export default IndexPage;
