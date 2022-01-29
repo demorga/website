617de829e661042ce77bf068d79a76666a5bd634
ztk37 <ztk37.contact@gmail.com>
2022-01-29 18:38:54 +0100

    update

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -5,7 +5,7 @@ import styles from "./HomepageFeatures.module.css";
 const FeatureList = [
   {
     title: "Unus",
-    // Svg: require('/path/to/svg').default,
+    Svg: require("../../public/assets/undraw_handcrafts_planet.svg").default,
     description: (
       <>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
@@ -16,7 +16,7 @@ const FeatureList = [
   },
   {
     title: "Duo",
-    // Svg: require('/path/to/svg').default,
+    Svg: require("../../public/assets/undraw_handcrafts_star.svg").default,
     description: (
       <>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
@@ -27,7 +27,7 @@ const FeatureList = [
   },
   {
     title: "Tres",
-    // Svg: require('/path/to/svg').default,
+    Svg: require("../../public/assets/undraw_handcrafts_space.svg").default,
     description: (
       <>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
@@ -42,7 +42,7 @@ function Feature({ Svg, title, description }) {
   return (
     <div className={clsx("col col--4")}>
       <div className="text--center">
-        {/* <Svg className={styles.featureSvg} alt={title} /> */}
+        <Svg className={styles.featureSvg} alt={title} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
